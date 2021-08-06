dac9114fe2b7e6a459e36b4fa1b04cf33dd830d2
piotrfijol <piotr.fijol1@gmail.com>
2021-08-06 12:48:15 +0200

    Added views

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container d-flex flex-column justify-content-center align-items-center">
+        <h1 class="app-name animate__animated animate__bounceIn">ToDoApp</h1>
+        <p class="main__caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <router-link to="/todo"><button class="btn btn-dark py-3 px-5">Get Started</button></router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home'
+    }
+</script>
+
+<style scoped>
+
+  .container {
+    height: 100vh;
+  }
+
+  .container .app-name {
+    font-size: 64px;
+  }
+
+  .main__caption {
+    font-size: 20px;
+    max-width: 520px;
+    word-wrap: break-word;
+  }
+
+</style>
