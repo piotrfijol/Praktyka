e93ddd1f1009c374371b826c630b85eba040ab87
piotrfijol <piotr.fijol1@gmail.com>
2021-08-06 12:44:11 +0200

    Added Tasks component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tasks">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tasks',
+    components: {
+
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
