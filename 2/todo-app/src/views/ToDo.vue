b6143e71281c39d502d7e825a47f51a8e57398d6
piotrfijol <piotr.fijol1@gmail.com>
2021-08-06 12:48:15 +0200

    Added views

---
+++ 2/todo-app/src/views/ToDo.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <add-task @addTask="addTask" />
+        <tasks v-bind:key="task.id" v-for="task in tasks">
+            <task @remove="removeTask" :taskId="task.id" :title="task.name" :date="task.date"/>
+        </tasks>
+    </div>
+</template>
+
+<script>
+    import Tasks from '../components/Tasks.vue';
+    import Task from '../components/Task.vue';
+    import AddTask from '../components/AddTask.vue';
+
+    export default {
+        name: 'todo',
+        data() {
+            return {
+                tasks: [
+                    {
+                        id: 1,
+                        name: 'Taskk',
+                        date: new Date()
+                    },
+                    {
+                        id: 2,
+                        name: 'Taskkkkk',
+                        date: new Date()
+                    }
+                ]
+            }
+        },
+        components: {
+            Tasks,
+            Task,
+            AddTask
+        },
+        methods: {
+            addTask({ name }) {
+                this.tasks.push({
+                    id: Math.floor(Math.random()*10000),
+                    date: new Date(),
+                    name
+                })
+            },
+            removeTask(taskId) {
+                console.log(taskId)
+                this.tasks = this.tasks.filter(task => task.id != taskId);
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    
+</style>
