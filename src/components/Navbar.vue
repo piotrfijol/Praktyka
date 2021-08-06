3a74cc9551271d0aedb36f2b79c37de3580d43bb
piotrfijol <piotr.fijol1@gmail.com>
2021-08-06 12:44:49 +0200

    Added Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar py-3">
+      <div class="container d-flex justify-content-between align-items-center">
+        <a href="/" class="nav-link text-dark"><h1 class="app-name">ToDoApp</h1></a>    
+        <a href="https://github.com/piotrfijol" class="nav-link"><img src="../assets/github_mark.svg" alt="github mark" class="github-link d-block"></a>
+      </div>
+    </nav>
+</template>
+
+<script>
+
+export default {
+    name: 'navbar'
+}
+</script>
+
+<style scoped> 
+  .app-name {
+    text-align: left;
+  }
+
+  .github-link {
+    height: 40px;
+  }
+  
+</style>
