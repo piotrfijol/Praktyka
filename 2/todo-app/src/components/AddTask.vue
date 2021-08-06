5fe29ae905845e38f969ab58d4a631b9f0b415ec
piotrfijol <piotr.fijol1@gmail.com>
2021-08-06 12:43:44 +0200

    Added AddTask component

---
+++ 2/todo-app/src/components/AddTask.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container mt-5">
+        <div class="input-group mb-5">
+            <input  @keypress="handleKeyPress" v-model="newTask.name" type="text" class="form-control py-3" placeholder="New task">
+            <button @click="addTask" class="btn btn-success px-4" type="button" id="button-addon2">Add</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'add-task',
+    data() {
+        return {
+            newTask: {
+                name: '',
+            }
+        }
+    },
+    methods: {
+        handleKeyPress(ev) {
+            if(ev.keyCode === 13) {
+                this.addTask();
+            }
+        },
+        addTask() {
+            this.$emit('addTask', this.newTask);
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .container {
+        max-width: 800px;
+    }
+</style>
